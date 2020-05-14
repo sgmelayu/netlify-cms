531136eec27bc99dcf99540450474857cbf84daf
erezrokah <erezrokah@users.noreply.github.com>
2020-05-14 14:07:48 +0300

    fix: only show entry when parent is selected

--- packages/netlify-cms-core/src/components/Collection/Collection.js
+++ packages/netlify-cms-core/src/components/Collection/Collection.js
@@ -15,7 +15,6 @@ import { sortByField } from 'Actions/entries';
 import { selectSortableFields } from 'Reducers/collections';
 import { selectEntriesSort } from 'Reducers/entries';
 import { VIEW_STYLE_LIST } from 'Constants/collectionViews';
-import { getFilterPath } from '../../routing/helpers';
 
 const CollectionContainer = styled.div`
   margin: ${lengths.pageMargin};
@@ -96,9 +95,8 @@ export class Collection extends React.Component {
     let newEntryUrl = collection.get('create') ? getNewEntryUrl(collectionName) : '';
     if (newEntryUrl && filterTerm) {
       newEntryUrl = getNewEntryUrl(collectionName);
-      const path = getFilterPath(filterTerm);
-      if (path) {
-        newEntryUrl = `${newEntryUrl}?path=${path}`;
+      if (filterTerm) {
+        newEntryUrl = `${newEntryUrl}?path=${filterTerm}`;
       }
     }
 
